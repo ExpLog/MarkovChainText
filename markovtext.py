9ab3f32b9299d5a8d502a0b33ad340daee8a40c2
lfontoura <lobo.fontoura@gmail.com>
2016-02-08 17:48:07 -0200

    Adds missing documentation.

--- markovtext.py
+++ markovtext.py
@@ -15,18 +15,23 @@ def sliding_window(iter, n=3):
 
 
 def is_capitalized(string):
-    if len(string) > 0:
-        return string[0].isupper()
-    else:
-        return False
+    """Checks if the word is capitalized. Expects non-empty strings"""
+    return string[0].isupper()
 
 
 def is_final_word(string):
+    """Checks if word ends with terminal ponctuation."""
     # TODO: this sucks, change into a regexp
-    return string[-1] in ".!?"
+    return string[-1] in ".!?:;"
 
 
 def discrete_sample(seq):
+    """
+    Samples from a discrete distribution.
+    Expects a sequence where each element is a 2-tuple, of the format (element, probability).
+    :param seq:
+    :return:
+    """
     rand = random.random()
     words, probability = zip(*seq)
     acc_words = zip(words, it.accumulate(probability))
@@ -95,7 +100,7 @@ class MarkovChainText(object):
 
 
 file = open("test/pg11.txt", "r")
-mc_text = MarkovChainText(file)
+mc_text = MarkovChainText(file, 3)
 
 # TODO: set random seed
-print(" ".join(mc_text.sample_phrases(2)))
+print(" ".join(mc_text.sample_phrases(1)))

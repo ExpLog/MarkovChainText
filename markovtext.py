c5fc18972a6bd49c9cdc417bca693570af18df07
lfontoura <lobo.fontoura@gmail.com>
2016-02-08 16:00:01 -0200

    Adds normalize_markov and markov_chain.
    The basic infrastructure to create a markov chain from text is ready.

--- markovtext.py
+++ markovtext.py
@@ -1,5 +1,6 @@
-import sys
 import itertools as it
+from collections import Counter
+import sys
 
 
 def sliding_window(iter, n=3):
@@ -12,9 +13,30 @@ def sliding_window(iter, n=3):
         yield window
 
 
-file = open(sys.argv[1], "r")
-text = it.chain(*[line.split(" ") for line in file])
+def normalize_markov(markov_chain):
+    """Normalizes a markov chain."""
+    total = {key: sum(map(lambda t: t[1], value)) for key, value in markov_chain.items()}
+    return {key: [(word, count/total[key]) for word, count in word_list]
+            for key, word_list in markov_chain.items()}
 
-history = 3
-for line in sliding_window(text, history):
-    print(line)
+
+def markov_chain(file, history=3):
+    """Creates a Markov Chain from a file with text, already normalized."""
+    text = it.chain(*[line.split(" ") for line in file])
+
+    history = 3
+    window_counter = Counter(sliding_window(text, history+1))
+
+    mc = {}
+    for window, count in window_counter.items():
+        curr_state = window[0:history]
+        next_state = window[-1]
+        if curr_state not in mc:
+            mc[curr_state] = [(next_state, count)]
+        else:
+            mc[curr_state].append((next_state, count))
+    return normalize_markov(mc)
+
+
+file = open(sys.argv[1], "r")
+text_chain = markov_chain(file, 3)
